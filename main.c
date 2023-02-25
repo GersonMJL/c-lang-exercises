927685176959310cb192ef550263edb68642301c
GersonMJL <gersonmjl@gmail.com>
2023-02-25 10:32:08 -0300

    Refactor and adding some comments

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Develop a C program that reads the height of people whose number of people is given by the user.
+// This program should check and show the highest and lowest height.
 int heights() {
     char input[20];
     int numberPeople;
-    int biggestHeight, smallestHeight = 0;
+    int highestHeight, lowestHeight = 0;
 
     printf("Insert number of people:\n");
     scanf("%s", input);
@@ -20,20 +22,20 @@ int heights() {
 
     for (int i = 0; i < numberPeople; ++i) {
         if (i == 0) {
-            smallestHeight = heights[i];
-            biggestHeight = heights[i];
+            lowestHeight = heights[i];
+            highestHeight = heights[i];
         } else {
-            if (heights[i] > biggestHeight) {
-                biggestHeight = heights[i];
+            if (heights[i] > highestHeight) {
+                highestHeight = heights[i];
             }
-            if (heights[i] < smallestHeight) {
-                smallestHeight = heights[i];
+            if (heights[i] < lowestHeight) {
+                lowestHeight = heights[i];
             }
         };
     }
 
-
-
+    printf("The biggest height is: %d\n", highestHeight);
+    printf("The smallest height is: %d\n", lowestHeight);
     return 0;
 }
 
@@ -57,7 +59,7 @@ int factorial() {
 }
 
 int main() {
-//    factorial();
+    factorial();
     heights();
     return 0;
 }
